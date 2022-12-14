2b84dcbfce1bccbacf697b32116f3d0d2e6916d2
Patrik Lie <patrik.lie@hotmail.com>
2022-12-14 11:28:31 +0100

    First random fighter generator

---
+++ index.js
@@ -0,0 +1,16 @@
+// Message generator
+
+const movies = ["Rocky", ]
+
+const rocky = ["Rocky Balboa", "Paulie", "Adrian", "Apollo Creed", "Mickey", "Mr. T", "Thunderlips", "Ivan Drago", "Tommy Gunn", "Mason Dixon"]
+
+let randomNumber1 = Math.floor(Math.random() * rocky.length)
+let randomNumber2 = Math.floor(Math.random() * rocky.length)
+
+while(randomNumber1 === randomNumber2){
+    randomNumber1 = Math.floor(Math.random() * rocky.length)
+}
+
+console.log(`Your fighter from the famous Rocky movies is ${rocky[randomNumber1]}!`)
+console.log(`Your opponent is... ${rocky[randomNumber2]}!`)
+console.log(`Do you think ${rocky[randomNumber1]} will win against ${rocky[randomNumber2]}?`)
